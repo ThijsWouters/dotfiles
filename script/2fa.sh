ae6cc7fb40842c65e62f28216c83d7dea35b3437
Thijs Wouters <thijs@morewood.be>
2018-10-14 11:50:52 +0200

    Inform user of what happened

--- script/2fa.sh
+++ script/2fa.sh
@@ -1,5 +1,6 @@
 if [ -f ~/$1 ]; then
   oathtool --totp -b $(cat ~/$1) | xclip -selection clipboard
+  echo "The 2fa code was copied onto the clipboard."
 else
   echo "The key file for $1 does not exist."
   echo "Create one in your home direcory."
